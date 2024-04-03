702ca439297c6e3c47a80b6a1e1995faf6f5f5ba
Guruprasad <guruprasad1736@gmail.com>
2024-04-03 19:16:29 +0530

    Done With Database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,11 @@ dependencies {
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+
+    //Room Database
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
 }
---
+++ app/src/main/java/com/guruprasad/foodapplication/DB/MealDatabase.kt
@@ -0,0 +1,33 @@
+package com.guruprasad.foodapplication.DB
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.guruprasad.foodapplication.Model.Meal
+
+@Database(entities = [Meal::class] , version = 1 , exportSchema = false)
+@TypeConverters(MealTypeConverter::class)
+abstract class MealDatabase : RoomDatabase() {
+
+    abstract fun mealDao(): MealDao
+
+    companion object{
+        @Volatile
+        var INSTANCE : MealDatabase?= null
+
+        @Synchronized
+        fun getInstance (context: Context) : MealDatabase{
+            if (INSTANCE == null)
+            {
+                INSTANCE = Room.databaseBuilder(context,MealDatabase::class.java,"Meal.db")
+                    .fallbackToDestructiveMigration().build()
+            }
+
+            return INSTANCE as MealDatabase
+        }
+    }
+
+
+}

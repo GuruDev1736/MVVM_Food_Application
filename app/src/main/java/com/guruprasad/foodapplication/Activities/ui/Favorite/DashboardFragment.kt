4b2758220cee2c6eb7813b9d810ad05d31016225
Guruprasad <guruprasad1736@gmail.com>
2024-04-10 00:14:58 +0530

    done with the slide to delete

--- app/src/main/java/com/guruprasad/foodapplication/Activities/ui/Favorite/DashboardFragment.kt
+++ app/src/main/java/com/guruprasad/foodapplication/Activities/ui/Favorite/DashboardFragment.kt
@@ -1,20 +1,25 @@
 package com.guruprasad.foodapplication.Activities.ui.Favorite
 
+import android.content.ClipData.Item
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.guruprasad.foodapplication.Activities.Adapters.FavMealAdapter
 import com.guruprasad.foodapplication.DB.MealDatabase
 import com.guruprasad.foodapplication.ViewModel.MealViewModel
 import com.guruprasad.foodapplication.ViewModel.MealViewModelFactory
 import com.guruprasad.foodapplication.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment() {
+class DashboardFragment() : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
     private lateinit var viewModel : MealViewModel
@@ -39,6 +44,8 @@ class DashboardFragment : Fragment() {
         observeFavorite()
 
 
+
+
         return root
     }
 
@@ -46,6 +53,40 @@ class DashboardFragment : Fragment() {
         viewModel.ObserveFavLiveData().observe(viewLifecycleOwner){
             adapter = FavMealAdapter(requireContext(),it)
             binding.recyclerView.adapter = adapter
+
+            val itemTouchHelper = object : ItemTouchHelper.SimpleCallback(
+                ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+                ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ) = true
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    val position = viewHolder.adapterPosition
+                    val mealList = it // Assuming it is the list of meals
+
+                    if (mealList.isNotEmpty() && position < mealList.size) {
+                        val deletedMeal = mealList[position]
+                        viewModel.deleteMeal(deletedMeal)
+
+                        Snackbar.make(requireView(), "Meal Deleted Successfully", Snackbar.LENGTH_LONG)
+                            .setAction("UNDO") { _ ->
+                                viewModel.insertMeal(deletedMeal)
+                            }
+                            .show()
+                    } else {
+                        Log.e("TAG", "Invalid position or meal list is empty")
+                    }
+                }
+            }
+
+
+            ItemTouchHelper(itemTouchHelper).attachToRecyclerView(binding.recyclerView)
+
+
+
         }
     }
 
